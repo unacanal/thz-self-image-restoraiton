6de876e8316724ba4e3c1494500fa4956e13df17
unacanal <woonhahaha@gmail.com>
2023-06-05 17:10:51 +0900

    feat: Add test.py file

---
+++ test.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+from net import ZSSRNet
+import torch
+from torch.autograd import Variable
+import PIL
+from torchvision import transforms
+import argparse
+
+
+def test(model, img, save_dir, save_name):
+    model.eval()
+
+    img = transforms.ToTensor()(img)
+    img = torch.unsqueeze(img, 0)
+    input = Variable(img.cuda())
+    residual = model(input)
+    output = input + residual
+
+    output = output.cpu().data[0, :, :, :]
+    o = output.numpy()
+    o[np.where(o < 0)] = 0.0
+    o[np.where(o > 1)] = 1.0
+    output = torch.from_numpy(o)
+    output = transforms.ToPILImage()(output) 
+    output.save(f'results/{save_dir}/{save_name}_zssr.png')
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_img', type=str, help='Path to test img', default=None, required=False)
+    parser.add_argument('--ckpt_path', type=str, help='Path to checkpoint')
+    parser.add_argument('--save_dir', type=str, help='Dir to save result')
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    
+    model = ZSSRNet(input_channels=1).cuda()
+    model.load_state_dict(torch.load(args.ckpt_path))
+    if not args.test_img:
+        # if args.test_img not exists
+        testset = np.arange(0.2, 1.2, 0.2)
+        testset2 = np.arange(1.2, 2.2, 0.2)
+        testset = np.append(testset, testset2)
+
+        for k in testset:
+            k = round(k, 2)
+            test_img_path =  f'thz/resol1951_2_e_{k}THz.png'
+            print(test_img_path)
+
+            test_img = PIL.Image.open(test_img_path)
+
+            test_img_name = os.path.splitext(os.path.basename(test_img_path))[0]
+            save_name = test_img_name + \
+                '('+''.join(os.path.splitext(os.path.basename(args.ckpt_path))[0].split('_')[-3:]).split('THz')[0]+')'
+                
+            test(model, test_img, args.save_dir, save_name)
+
+    else:
+        test_img = PIL.Image.open(args.test_img)
+
+        test_img_name = os.path.splitext(os.path.basename(args.test_img))[0]
+        save_name = test_img_name + \
+            '('+ os.path.basename(args.ckpt_path).split('THz')[0] + ')'
+            
+        test(model, test_img, args.save_dir, save_name)
