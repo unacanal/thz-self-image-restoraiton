657c58dafee8b3cf743ffb5f2f1282ab3427b37d
unacanal <woonhahaha@gmail.com>
2023-06-22 19:07:52 +0900

    [feat] Add code for loading new model CAIR

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from net import ZSSRNet
+from net import ZSSRNet, CAIR
 import torch
 from torch.autograd import Variable
 import PIL
@@ -22,8 +22,8 @@ def test(model, img, save_dir, save_name):
     o[np.where(o < 0)] = 0.0
     o[np.where(o > 1)] = 1.0
     output = torch.from_numpy(o)
-    output = transforms.ToPILImage()(output) 
-    output.save(f'results/{save_dir}/{save_name}_zssr.png')
+    output = transforms.ToPILImage()(output)
+    output.save(f'results/{save_dir}/{save_name}.png')
 
 
 def get_args():
@@ -31,6 +31,7 @@ def get_args():
     parser.add_argument('--test_img', type=str, help='Path to test img', default=None, required=False)
     parser.add_argument('--ckpt_path', type=str, help='Path to checkpoint')
     parser.add_argument('--save_dir', type=str, help='Dir to save result')
+    parser.add_argument('--model', type=str, help='Model name')
 
     args = parser.parse_args()
 
@@ -40,7 +41,14 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     
-    model = ZSSRNet(input_channels=1).cuda()
+    if args.model == 'CAIR':
+        model = CAIR(
+                    img_channel=1, width=16, middle_blk_num=1,
+                    enc_blk_nums=[1, 1, 1, 1], dec_blk_nums=[1, 1, 1, 1]
+                ).cuda()
+    else:
+        model = ZSSRNet(input_channels=1).cuda()
+
     model.load_state_dict(torch.load(args.ckpt_path))
     if not args.test_img:
         # if args.test_img not exists
